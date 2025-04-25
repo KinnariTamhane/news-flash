79e57f5a09f8d9f5253718da0f6b62e98605e99c
Kinnari <kinnari.tamhane@tickledmedia.com>
2025-04-25 12:55:11 +0530

    update ui for news

--- newsflash/src/app/layout.tsx
+++ newsflash/src/app/layout.tsx
@@ -19,6 +19,7 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({
+
   children,
 }: Readonly<{
   children: React.ReactNode;
@@ -26,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-red`}
       >
         <Header/>
         {children}
